c5108aca85f3182b0b0b0ffed7db0afba9a5c8c4
jhj9109 <36416495+jhj9109@users.noreply.github.com>
2023-04-16 13:47:44 +0000

    Update: 약간의 추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from 'react';
 import './App.css';
 import axios, { AxiosResponse } from 'axios';
 
+type AxiosMethods = 'get' | 'post' | 'put' | 'delete' | 'patch' | 'options' | 'head'
+
 interface UrlObject {
   url: string;
-  method: 'get' | 'post';
+  method: AxiosMethods;
   data: any;
 }
 
@@ -13,22 +15,13 @@ interface Result {
   res?: AxiosResponse<any, any>;
 }
 
-function App() {
-  const [result, setResult] = useState<Result>(() => ({url: ''}))
-  const requestApi = async (urlObj: UrlObject) => {
-    const { url, method, data } = urlObj
-    const res = await axios({ url, method, data })
-    setResult(() => ({url, res}))
-  }
-  const urls: UrlObject[] = [
-    { url: "/api/products", method: 'get', data: null },
-    { url: "/api/login", method: 'post', data: {username: "jhj"} },
-  ]
-  useEffect(() => {
-    if (result.res) {
-      console.log(result.res)
-    }
-  })
+interface AppBoxProps {
+  urls: UrlObject[];
+  result: Result;
+  requestApi: (urlObj: UrlObject) => Promise<void>;
+}
+
+const AppBox = ({urls, result, requestApi}: AppBoxProps) => {
   return (
     <div className="App">
       <div className="top-section">
@@ -46,6 +39,31 @@ function App() {
         <p>data: {JSON.stringify(result.res?.data) ?? ''}</p>
       </div>
     </div>
+  );
+}
+
+const App = () => {
+  const [result, setResult] = useState<Result>(() => ({url: ''}))
+  const requestApi = async (urlObj: UrlObject) => {
+    const { url, method, data } = urlObj
+    const res = await axios({ url, method, data })
+    setResult(() => ({url, res}))
+  }
+  const urls: UrlObject[] = [
+    { url: "/api/products", method: 'get', data: null },
+    { url: "/api/login", method: 'post', data: {username: "jhj"} },
+  ]
+  useEffect(() => {
+    if (result.res) {
+      console.log(result.res)
+    }
+  })
+  return (
+    <AppBox
+      urls={urls}
+      result={result}
+      requestApi={requestApi}
+    />
   );
 }
 
